56d76fa2d7148effb5780c0f6d18919ddf5e3c09
sw-lee-dev <lsw6212@naver.com>
2025-02-27 17:16:43 +0900

    2025.02.27 17:50

--- src/main/java/com/korit/basic/config/CorsConfig.java
+++ src/main/java/com/korit/basic/config/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // Cors 정책 : Cross Origin Resource Sharing으로 웹 브라우저의 스크립트로 출처가 서로 다른 곳으로 자원을 공유할 때 지정된 정책
 // - 출처(Origin) : 프로토콜, 호스트, 포트가 출처에 해당함
-// - 출처가 서로 다른 위치에 스크립트로 요청을 보내면 일반적으로 CORS 정책에 의해 막힘\
+// - 출처가 서로 다른 위치에 스크립트로 요청을 보내면 일반적으로 CORS 정책에 의해 막힘
 
 // @Configuration : 생성자나 메서드가 호출 시 Spring Bean으로 등록될 수 있도록 하는 어노테이션
 @Configuration
---
+++ src/main/java/com/korit/basic/controller/ServiceController.java
@@ -0,0 +1,59 @@
+package com.korit.basic.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.korit.basic.service.BasicService;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/service")
+// 생성자를 이용한 의존성 주입 방법은 @Autowired를 생략할 수 있기 때문에 lombok으로 생성자를 자동 생성하여 응용할 수 있음
+// @AllArgsConstructor
+// 만약, 의존성을 선택적으로 주입하겠다 한다면 의존성을 final로 지정하여 @RequiredArgsConstructor를 이용할 수 있음
+@RequiredArgsConstructor
+public class ServiceController {
+
+  // 의존성 주입 (DI) :
+  // - 해당 모듈에 필요한 의존성을 클래스의 인스턴스를 생성하는 위치에서 주입하는 것
+  // - Spring framework에서 @Autowired를 이용하여 의존성 주입을 Spring에게 위임할 수 있음 (IoC, DI)
+  // - 필드를 이용한 의존성 주입, setter 메서드를 이용한 의존성 주입, 생성자를 이용한 의존성 주입
+  // - 생성자를 이용한 의존성 주입 방법이 가장 안정적이므로 Spring framework에서는 생성자를 이용한 의존성 주입을 권장
+  // - 주의! : IoC를 통해 Spring에게 의존성 주입을 의임하려면 주입하려는 클래스가 Spring Bean으로 등록(@Component)되어 있어야함
+
+  // @Autowired : 의존성 주입을 Spring에 위임
+  // - 주입할 의존성이 반드시 Spring Bean으로 등록되어 있어야함
+
+  // 1.필드를 이용한 의존성 주입 방법
+  // @Autowired
+  // private BasicService basicService;
+
+  // 2.setter 메서드를 이용한 의존성 주입 방법
+  // private BasicService basicService;
+
+  // @Autowired
+  // public void setBasicService(BasicService basicService) {
+  //   this.basicService = basicService;
+  // }
+
+  // 3.생성자를 이용한 의존성 주입 방법
+  // *권장사항*
+  private final BasicService basicService;
+
+  // 생성자를 이용한 의존성 주입 방법을 사용할 땐 @Autowired를 생략해도 됨(권장사항이기 때문)
+  // @Autowired(생략 가능)
+  // public ServiceController(BasicService basicService) {
+  //   this.basicService = basicService;
+  // }
+  
+  @GetMapping("")
+  public ResponseEntity<String> getService() {
+    return basicService.getService();
+  }
+
+}
